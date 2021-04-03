f7f885fff46931659badfdf7583c00e2d7edbc4e
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-04-03 23:32:53 +0300

    Update FilmsController

--- films.api/Controllers/FilmsController.cs
+++ films.api/Controllers/FilmsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using films.api.Models.Mongo;
+using films.api.Services.Mongo;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,6 +13,57 @@ namespace films.api.Controllers
     [Route("api/[controller]")]
     public class FilmsController : ControllerBase
     {
-        
+        private readonly MongoService _service;
+        public FilmsController(MongoService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Films>> Get() => _service.GetAll();
+
+        [HttpGet("{id:length(24)}", Name = "GetById")]
+        public ActionResult<Films> Get(string id)
+        {
+            var film = _service.GetById(id);
+
+            if (film == null)
+                return NotFound();
+
+            return film;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Films model)
+        {
+            _service.Add(model);
+            return CreatedAtRoute("GetById", new { id = model.Id }, model);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Films model)
+        {
+            var film = _service.GetById(id);
+
+            if (film == null)
+                return NotFound();
+
+            _service.Update(id, model);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var film = _service.GetById(id);
+
+            if (film == null)
+                return NotFound();
+
+            _service.Delete(film);
+
+            return NoContent();
+        }
     }
 }
