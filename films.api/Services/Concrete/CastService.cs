7164061c1a9bdfa902d64807324665fa88d209e2
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-04-04 22:58:49 +0300

    Add CastService and update files

---
+++ films.api/Services/Concrete/CastService.cs
@@ -0,0 +1,16 @@
+﻿using films.api.Models;
+using films.api.Services.Mongo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace films.api.Services
+{
+    public class CastService : GenericService<Cast>
+    {
+        public CastService(IMongoDatabaseSettings settings) : base(settings)
+        {
+        }
+    }
+}
