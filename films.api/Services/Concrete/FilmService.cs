9b8ab84e2bedd92811520e149ef335079e668936
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-04-04 22:56:46 +0300

    Rename MongoService to FilmService

---
+++ films.api/Services/Concrete/FilmService.cs
@@ -0,0 +1,17 @@
+﻿using films.api.Models.Mongo;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace films.api.Services.Mongo
+{
+    public class FilmService : GenericService<Films>
+    {
+        public FilmService(IMongoDatabaseSettings settings) : base(settings)
+        {
+        }
+
+    }
+}
