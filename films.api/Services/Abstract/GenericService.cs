8b2245be6d972ca7a81bd1a695fc6f42a058c979
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-04-04 22:54:45 +0300

    Add GenericService and IMainService

---
+++ films.api/Services/Abstract/GenericService.cs
@@ -0,0 +1,38 @@
+﻿using films.api.Models;
+using films.api.Services.Mongo;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace films.api.Services
+{
+    public class GenericService<TEntity> : IMainService<TEntity, string>
+        where TEntity: BaseModel
+    {
+        private readonly IMongoCollection<TEntity> _collection;
+
+        public GenericService(IMongoDatabaseSettings settings)
+        {
+
+            var client = new MongoClient(settings.ConnectionString);
+            var db = client.GetDatabase(settings.DatabaseName);
+
+            _collection = db.GetCollection<TEntity>(typeof(TEntity).Name);
+        }
+
+        
+
+        public TEntity GetById(string id) => _collection.Find(x => x.Id == id).FirstOrDefault();
+        
+        public List<TEntity> GetAll() => _collection.Find(e => true).ToList(); 
+        
+        public void Add(TEntity entity) => _collection.InsertOne(entity);
+
+        public void Delete(TEntity entity) => _collection.DeleteOne(x => x.Id == entity.Id);
+
+        public void Update(string id, TEntity entity) => _collection.ReplaceOne(x => x.Id == id, entity);
+    }
+
+}
---
+++ films.api/Services/Abstract/IMainService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace films.api.Services
+{
+    public interface IMainService<T, TKey>
+        where TKey: IEquatable<TKey>
+    {
+        List<T> GetAll();
+        T GetById(TKey id);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(TKey id, T entity);
+    }
+}
