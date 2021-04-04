9d8fa957e6d18b42b498f034367323fb0fa632d0
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-04-04 22:53:28 +0300

    Add BaseModel and IEntity

---
+++ films.api/Models/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿using films.api.Services;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace films.api.Models
+{
+    public class BaseModel : IEntity<string>
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+    }
+}
---
+++ films.api/Services/Abstract/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace films.api.Services
+{
+    public interface IEntity
+    {
+
+    }
+
+    public interface IEntity<out TKey> : IEntity 
+        where TKey : IEquatable<TKey>
+    {
+        public TKey Id { get; }
+    }
+}
