34505c723b4c43a10d91792b07e6d1ae2bb0ff66
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-04-11 15:16:14 +0300

    Add FilmsDTO and MapperProfiles

---
+++ films.api/DTO/FilmsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace films.api.DTO
+{
+    public class FilmsDTO
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public double Imdb { get; set; }
+    }
+}
---
+++ films.api/Helpers/MapperProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using films.api.DTO;
+using films.api.Models.Mongo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace films.api.Helpers
+{
+    public class MapperProfiles: Profile
+    {
+        public MapperProfiles()
+        {
+            CreateMap<Films, FilmsDTO>();
+        }
+    }
+}
