3257a9debe0a13c16cc4d6b53ae4502ef481a9a0
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-05-04 09:42:41 +0300

    Update Startup.cs

--- films.api/Startup.cs
+++ films.api/Startup.cs
@@ -17,6 +17,7 @@ using System.Text.Json;
 using System.Linq;
 using System.Net.Mime;
 using Microsoft.AspNetCore.Http;
+using films.api.Extensions;
 
 namespace films.api
 {
@@ -29,7 +30,6 @@ namespace films.api
 
         public IConfiguration Configuration { get; }
 
-        readonly string MyAllowOrigins = "_myAllowOrigins";
         public void ConfigureServices(IServiceCollection services)
         {
             // Problems
@@ -40,62 +40,22 @@ namespace films.api
 
             var mongoDbSettings = Configuration.GetSection(nameof(MongoDatabaseSettings));
             services.Configure<MongoDatabaseSettings>(mongoDbSettings);
-            services.AddSingleton<IMongoDatabaseSettings>(options => options.GetRequiredService<IOptions<MongoDatabaseSettings>>().Value);
 
-            #region Swagger
-            services.AddSwaggerGen(s =>
-            {
-                s.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "Films API",
-                    Description = "ASP Net Core Films API. Mongo DB and Generic Repository.",
-                    TermsOfService = new Uri("https://www.cancanbolat.site"),
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Can Canbolat",
-                        Email = "canxmcclyn@gmail.com"
-                    }
-                });
+            services.ConfigureDependency(); //Dependency Extension
 
-                // XML
-                var xmlFile = $"{ Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                s.IncludeXmlComments(xmlPath);
-            });
-            #endregion
+            services.ConfigureSwagger(); //Swagger Extension
+            
+            services.ConfigureCache(); //Cache Extension
 
-            #region Life Cycles
-            services.AddScoped<FilmService>();
-            services.AddScoped<CastService>();
-            #endregion
+            services.ConfigureCors(); //CORS Extension
 
-            services.AddResponseCaching(); // response cache
-
-            services.AddControllers(op =>
-                op.CacheProfiles.Add("Duration20Cache", new CacheProfile { Duration = 20 })
-            ).AddNewtonsoftJson();
-
-            #region CORS
-            services.AddCors(options =>
-            {
-                options.AddPolicy(
-                    name: MyAllowOrigins,
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
-                    });
-            });
-            #endregion
-
-            #region Health Checks
+            //Health Check
             services.AddHealthChecks().AddMongoDb(
                     Configuration["MongoDatabaseSettings:ConnectionString"],
                     name: "FilmsApi",
                     timeout: TimeSpan.FromSeconds(3),
                     tags: new[] { "ready" }
-                );
-            #endregion
+             );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -113,46 +73,20 @@ namespace films.api
 
             app.UseRouting();
 
-            app.UseCors(MyAllowOrigins); //cors
-            app.UseResponseCaching(); //cache
+            app.UseCors("_myAllowOrigins");
+            app.UseResponseCaching();
 
-            #region Swagger
             app.UseSwagger();
             app.UseSwaggerUI();
-            #endregion
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-
-                #region Health Checks
-                endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
-                {
-                    Predicate = (check) => check.Tags.Contains("ready"),
-                    ResponseWriter = async (context, report) =>
-                    {
-                        var result = JsonSerializer.Serialize(
-                            new
-                            {
-                                status = report.Status.ToString(),
-                                checks = report.Entries.Select(e => new
-                                {
-                                    name = e.Key,
-                                    status = e.Value.Status.ToString(),
-                                    exception = e.Value.Exception != null ? e.Value.Exception.Message : "none",
-                                    duration = e.Value.Duration
-                                })
-                            }
-                        );
-
-                        context.Response.ContentType = MediaTypeNames.Application.Json;
-                        await context.Response.WriteAsync(result);
-                    }
-                });
-                #endregion
             });
+
+            app.UseCustomHealthChecks(); //HealthCheck Extension
         }
     }
 }
